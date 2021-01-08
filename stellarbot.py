07217c24f4a91a5cb7a6c11680c100190b8eae82
Taster98 <luigi.gesuele98@gmail.com>
2021-01-08 13:17:09 +0100

    First commit | Initialized all stuffs for python setup

---
+++ stellarbot.py
@@ -0,0 +1,41 @@
+import urllib, requests
+import config
+import re
+from bs4 import BeautifulSoup
+from twisted.internet import task, reactor
+
+oldvalue = 0.0
+def magicUpdateScraper():
+    tosend = False
+    # inizializzo la roba per la valuta
+    url = config.getScrapSite()
+    html = requests.get(url,headers={'Cache-Control': 'no-cache'}).text
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.findAll('div', {'class':'calculated-rate'})
+    valore = str(links)[293:300]
+    converted = "NUOVE STATISTICHE DI STELLAR: \n 1 XLM = "+valore +" €"
+    global oldvalue
+    if(oldvalue == 0.0):
+        oldvalue = float(valore)
+        tosend = True
+    if(abs(oldvalue-float(valore)) >= 0.02):
+        tosend = True
+        oldvalue = float(valore)
+    if(tosend):
+        url2 = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (
+    config.getToken(), config.getId(),converted)
+        _ = requests.get(url2, timeout=10)
+    print(valore)
+    
+
+# polling timeout
+timeout = 3600.0 #1 minuto
+
+def doWork():
+    magicUpdateScraper()
+    pass
+
+l = task.LoopingCall(doWork)
+l.start(timeout) # call ogni minuto
+
+reactor.run()
