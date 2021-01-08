030a7c27f0b53cb4a7473dc92d74b0f8b7f4d332
Taster98 <luigi.gesuele98@gmail.com>
2021-01-08 14:20:10 +0100

    Added Dollar to Eur conversion and chat interpreter

--- stellarbot.py
+++ stellarbot.py
@@ -1,7 +1,5 @@
 import urllib, requests
 import config
-import telepot
-from telepot.loop import MessageLoop
 import re
 from bs4 import BeautifulSoup
 from twisted.internet import task, reactor
@@ -29,33 +27,16 @@ def magicUpdateScraper():
     config.getToken(), config.getId(),converted)
         _ = requests.get(url2, timeout=10)
     print(valore)
-    
-# Robe per USD to EUR
-def usdToEurScraper():
-    url3 = "https://transferwise.com/it/currency-converter/usd-to-eur-rate"
-    html = requests.get(url3).text
-    soup = BeautifulSoup(html, "html.parser")
-    links = soup.findAll('span', {'class':'text-success'})
-    converted = str(links[0])[27:31]
-    return "Attualmente, 1€ = "+converted
-
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type == "text":
-        if(msg["text"] == "/dollaro" or msg["text"] == "/dollaro@updatestlrbot"):
-            bot.sendMessage(chat_id, usdToEurScraper())
 
 # polling timeout
-timeout = 3600.0 #1 minuto
-# Gestione chat e comandi
-bot = telepot.Bot(config.getToken)
-bot.message_loop(on_chat_message)
+timeout = 18000.0 + 3600.0 + 3600.0 #7 minuti
+
 print ('Listening ...')
 def doWork():
     magicUpdateScraper()
     pass
 
 l = task.LoopingCall(doWork)
-l.start(timeout) # call ogni minuto
+l.start(timeout) # call ogni 7 minuti
 
 reactor.run()
